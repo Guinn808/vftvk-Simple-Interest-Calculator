5a89f7390652356b4e3740ed17284a4f47d91764
Guinn808 <44260460+Guinn808@users.noreply.github.com>
2021-08-08 20:44:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -20,5 +20,5 @@ function compute()
 }
 
 function rangeDisplay() {
-    document.getElementById("range-number").innerHTML = document.getElementById("rate").value + "%";
+    document.getElementById("rate_val").innerHTML = document.getElementById("rate").value + "%";
 }
