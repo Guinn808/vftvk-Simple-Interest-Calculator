acddb186f26ccf9355b3048fc9a26d507389527b
Guinn808 <44260460+Guinn808@users.noreply.github.com>
2021-08-08 20:29:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,24 @@
 function compute()
 {
-    var principal = document.getElementById("principal").value;
-    var rate = document.getElementById("rate").value;
-    var years = document.getElementById("years").value;
+    let principal = parseFloat(document.getElementById("principal").value);
+    let rate = parseFloat(document.getElementById("rate").value);
+    let years = parseInt(document.getElementById("years").value);
+    let interest = principal * years * rate / 100;
+    let currentYear = new Date().getFullYear();
+    let futureYear = currentYear + years;
 
-    
-    
-    if (principal == "") { 
-        alert("Amount must not be empty");
+    if (isNaN(principal) || principal <= 0) {
+        alert("Enter pos. number");
         document.getElementById("principal").focus();
-        return false;
-    }else{
-        if (principal <= 0) { 
-            alert("Enter pos. number");
-            document.getElementById("principal").focus();
-            return false;
-        }
+    } else {
+        document.getElementById("result").innerHTML =
+            "     If deposit <mark>" + principal + "</mark>,<br>\n" +
+            "     at interest rate <mark>" + rate + "%</mark>.<br>\n" +
+            "     You receive an amount of <mark>" + interest + "</mark>,<br>\n" +
+            "     in year <mark>" + futureYear + "</mark>";
     }
-
-    var temp = new Date().getFullYear();
-    var future= parseInt(temp,10) + parseInt(years,10);
-    var interest= principal * years * rate / 100;
-    var result= "If You deposit <mark>" + principal + "</mark>,<br> at interest rate of <mark>" + rate + "</mark> % <br> You get an amount of <mark>" + interest + ",</mark><br> in year <mark>" + future + "</mark>";
-    document.getElementById("result").innerHTML=result;
 }
 
-function showVal(newVal){ //Show range value in span
-    document.getElementById("showRate").innerHTML=newVal + '%';
+function rangeDisplay() {
+    document.getElementById("range-number").innerHTML = document.getElementById("rate").value + "%";
 }
